0272594caec5c4ae03b3f2f82b3ed52ff11794f0
Jannis Gebauer <ja.geb@me.com>
2017-05-15 16:22:54 +0200

    added support for django 1.11

--- pyup_django/settings.py
+++ pyup_django/settings.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.conf import BaseSettings, settings as django_settings
-
-
-class Settings(BaseSettings):
+class Settings(object):
 
     API_URL = "https://pyup.io/api/v1/insecure/django/"
 
